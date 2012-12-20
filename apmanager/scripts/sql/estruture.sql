3a62d121d9d7c53de4d4d35dac00c45576ed4b75
Luis Carlos Boch <luis.c.boch@gmail.com>
2012-12-20 10:06:05 -0200

    atualização do script de estrutura.

--- apmanager/scripts/sql/estruture.sql
+++ apmanager/scripts/sql/estruture.sql
@@ -1,4 +1,4 @@
-﻿-- Table: marcas_de_produtos
+-- Table: marcas_de_produtos
 
 -- DROP TABLE marcas_de_produtos;
 
@@ -15,4 +15,42 @@ WITH (
   OIDS=FALSE
 );
 ALTER TABLE marcas_de_produtos
+  OWNER TO postgres;
+
+-- Table: shelf
+
+-- DROP TABLE shelf;
+
+CREATE TABLE shelf
+(
+  id serial NOT NULL,
+  code character varying(255),
+  description character varying(255),
+  registerdate timestamp without time zone,
+  status boolean,
+  CONSTRAINT shelf_pkey PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE shelf
+  OWNER TO postgres;
+
+-- Table: marcas_de_veiculos
+
+-- DROP TABLE marcas_de_veiculos;
+
+CREATE TABLE marcas_de_veiculos
+(
+  id serial NOT NULL,
+  descricao character varying(255),
+  nome character varying(255) NOT NULL,
+  data_do_registro timestamp without time zone NOT NULL,
+  status boolean,
+  CONSTRAINT marcas_de_veiculos_pkey PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE marcas_de_veiculos
   OWNER TO postgres;
