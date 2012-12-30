228b4859fd5b90f54a4923d165511424126df0f5
Luis Carlos Boch <luis.c.boch@gmail.com>
2012-12-30 14:41:23 -0200

    melhorado a validação ao gerar códigos adicionais.

--- apmanager/apmanager-service/src/main/java/com/apmanager/service/impl/ProductService.java
+++ apmanager/apmanager-service/src/main/java/com/apmanager/service/impl/ProductService.java
@@ -18,59 +18,65 @@ public class ProductService extends BasicService<Product, ProductDAO> {
     public ProductService() {
         this.dao = new ProductDAO(emanager);
     }
-    
-    
+
     @Override
     public void validate(Product object) throws ValidationException {
         ValidationException v = new ValidationException();
-        
-        
-        if(object.getName() == null || object.getName().equals("")){
+
+
+        if (object.getName() == null || object.getName().equals("")) {
             v.addError("Nome do produto inválido", "brand",
                     "invalid.product.name");
         }
-        
-        if(object.getBrand() == null){
-            v.addError("Seleciona a marca do produto", "brand", 
+
+        if (object.getBrand() == null) {
+            v.addError("Seleciona a marca do produto", "brand",
                     "invalid.product.brand");
         }
-        
-        if(object.getShelf() == null){
-            v.addError("Prateleira do produto inválida", "shelf", 
+
+        if (object.getShelf() == null) {
+            v.addError("Prateleira do produto inválida", "shelf",
                     "invalid.product.shelf");
         }
-        
-        if(object.getMinQuantity() == null || object.getMinQuantity().intValue() < 0){
-            v.addError("Quantidade minima do produto inválida", "minQuantity", 
+
+        if (object.getMinQuantity() == null || object.getMinQuantity().intValue() < 0) {
+            v.addError("Quantidade minima do produto inválida", "minQuantity",
                     "invalid.product.min.quantity");
         }
-        
-        if(object.getQuantity() == null){
-            v.addError("Quantidade do produto inválida", "quantity", 
+
+        if (object.getQuantity() == null) {
+            v.addError("Quantidade do produto inválida", "quantity",
                     "invalid.product.quantity");
         }
-        
-        if(object.getSellPrice() == null || object.getSellPrice().floatValue() < 0) {
-            v.addError("Preço de venda do produto inválido", "sellPrice", 
+
+        if (object.getSellPrice() == null || object.getSellPrice().floatValue() < 0) {
+            v.addError("Preço de venda do produto inválido", "sellPrice",
                     "invalid.product.sell.price");
         }
-        
-        if(object.getPurchuasePrice() == null || object.getPurchuasePrice().floatValue() < 0 ){
-            v.addError("Preço de Compra do produto inválido", "purchuasePrice", 
+
+        if (object.getPurchuasePrice() == null || object.getPurchuasePrice().floatValue() < 0) {
+            v.addError("Preço de Compra do produto inválido", "purchuasePrice",
                     "invalid.product.purchuase.price");
         }
-        
-        if(object.getMaxDiscountPercent() == null){
+
+        if (object.getMaxDiscountPercent() == null) {
             v.addError("Desconto máximo inválido", "maxDiscountPercent",
                     "invalid.product.discount.percent");
         }
-        
-        if (!v.isEmpty()){
+
+        if (!v.isEmpty()) {
             throw v;
         }
     }
-    
-    public synchronized void generateNewCode(Product product) throws Exception{
+
+    public synchronized void generateNewCode(Product product) throws Exception {
+        if (product.getAdditionalCode() != null && 
+                !product.getAdditionalCode().equals("")) {
+            final ValidationException ex = new ValidationException();
+            ex.addError("Este produto já possui código adicional",
+                    "additionalCode", "product.have.additional.code");
+            throw ex;
+        }
         String code = this.dao.getNextValidCode();
         product.setAdditionalCode(code);
         this.save(product);
